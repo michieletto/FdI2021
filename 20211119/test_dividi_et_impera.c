48bbd0b3f622f149f0a353bd5eff38e2a7340f1e
Stefano Michieletto <stefano.michieletto@gmail.com>
2021-11-24 14:23:39 +0100

    Create test_dividi_et_impera.c

---
+++ 20211119/test_dividi_et_impera.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int withdraw(int amount) {
+    /// ...
+}
+
+int main() {
+    bool condition = true;
+
+    while(condition) {
+        withdraw(0);
+        // update condition
+    }
+
+    return 0;
+}
