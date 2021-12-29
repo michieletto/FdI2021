cfb4d86ccbc6a0e8dae0afcdb0ecc71f8cfe519d
Stefano Michieletto <stefano.michieletto@unipd.it>
2021-12-29 11:59:44 +0100

    Adding example to mergesort

--- 20211220/merge_sort.c
+++ 20211220/merge_sort.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int *v, int dim) {
+	printf("\n[");
+	for (int i=0; i < dim - 1; i++)
+	{
+		printf(" %d,", v[i]);
+	}
+	printf(" %d]\n", v[dim-1]);
+}
+
+
 /* Function to merge the two sub-vector 
    v[l..m] and v[m+1..r] into v[l..r] */
 void merge(int *v, int l, int m, int r)
@@ -5,7 +18,7 @@ void merge(int *v, int l, int m, int r)
   int n1 = m - l + 1, n2 =  r - m;
   /* create temp array */
   int *tmp_v1=(int*)malloc(n1*sizeof(int)); 
-      *tmp_v2=(int*)malloc(n2*sizeof(int)); 
+  int *tmp_v2=(int*)malloc(n2*sizeof(int)); 
 
   /* Copy data to temp  tmp_v1[] and tmp_v2[] */
   for(int i = 0; i < n1; i++) 
@@ -50,4 +63,19 @@ void mergeSort(int *v, int l, int r)
     // … and merge
     merge(v, l, m, r);
   }
+}
+
+
+int main(void) {
+  int v[] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5};
+
+  printf("Pre-order:");
+	printArray(v, sizeof(v)/sizeof(v[0]));
+
+	mergeSort(v, 0, sizeof(v)/sizeof(v[0]));
+
+	printf("\nPost-order:");
+	printArray(v, sizeof(v)/sizeof(v[0]));
+
+  return 0;
 }
